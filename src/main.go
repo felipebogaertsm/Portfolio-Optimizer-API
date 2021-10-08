b037827d74da58c5ef014667882a3312f959be88
felipebogaerts <felipebogaerts@gmail.com>
2021-10-08 09:06:41 -0300

    1.0.3

--- src/main.go
+++ src/main.go
@@ -6,8 +6,14 @@ import (
 )
 
 func main() {
+	var sum float64
+
+	for i := 0; i < 1000; i++ {
+		sum = sum + float64(i)
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello World")
+		n, err := fmt.Fprintf(w, fmt.Sprintf("%.2f", sum))
 		if err != nil {
 			fmt.Println(err)
 		}
@@ -15,5 +21,6 @@ func main() {
 	})
 
 	// Start web server:
+	fmt.Println("Starting web server...")
 	http.ListenAndServe(":8080", nil)
 }
