9a660565aeefdb719388f7ac09cd9f5b969c2535
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2021-10-16 15:23:25 -0300

    0.1.6

--- src/main.go
+++ src/main.go
@@ -3,33 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"text/template"
+	"webapi/views"
 )
 
 const portNumber string = "8080"
 
-// Home is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
-}
-
-// About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("Error parsing template: ", err)
-	}
-}
-
 // main is the main function
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", views.Home)
+	http.HandleFunc("/about", views.About)
 
 	// Start web server:
 	fmt.Println(fmt.Sprintf("Starting web server on port %s...", portNumber))
