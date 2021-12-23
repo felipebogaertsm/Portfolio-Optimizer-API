c678a61a6de11d7eb4af32de7b38a1cb68227b6b
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2021-12-23 17:58:23 -0300

    0.1.13
    Finished covariance calculation finction

--- src/utils/calculations.go
+++ src/utils/calculations.go
@@ -24,18 +24,37 @@ func GetAnualisedReturns(df dataframe.DataFrame) dataframe.DataFrame {
 	return df
 }
 
-func GetCovariance(df dataframe.DataFrame) [][]float64 {
+func GetMean(df dataframe.DataFrame) dataframe.DataFrame {
+	df = df.Drop(0) // deleting date column
+
+	df = df.Capply(func(s series.Series) series.Series {
+		seriesFloat := s.Float()
+		mean := stat.Mean(seriesFloat, nil)
+		return series.Floats(mean)
+	})
+
+	return df
+}
+
+// Source:
+// https://www.investopedia.com/articles/financial-theory/11/calculating-covariance.asp
+func GetCovariance(df dataframe.DataFrame) dataframe.DataFrame {
 	df = df.Drop(0)
-
-	var covarianceMatrix [][]float64
-
 	colNames := df.Names()
-	for i := 0; i < len(colNames); i++ {
-		var covarianceMatrixRow []float64
-		for j := 0; j < len(colNames); j++ {
-			covarianceMatrixRow = append(covarianceMatrixRow, 0.3)
-		}
-	}
 
-	return covarianceMatrix
+	covarianceDf := df.Capply(func(s series.Series) series.Series {
+		var covarianceDfCol []float64
+		for i := 0; i < len(colNames); i++ {
+			colYName := colNames[i]
+			sum := 0.0 // sum of all elements
+			// Iterate through all elements of the series (col)
+			for j := 0; j < s.Len(); j++ {
+				sum += (s.Elem(j).Float() - s.Mean()) * (df.Col(colYName).Elem(j).Float() - df.Col(colYName).Mean())
+			}
+			covarianceDfCol = append(covarianceDfCol, sum/(float64(len(colNames)-1)))
+		}
+		return series.Floats(covarianceDfCol)
+	})
+
+	return covarianceDf
 }
